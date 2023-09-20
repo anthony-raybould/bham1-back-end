f3b4971c3172ae7f0eeaa5f03284cce2ec42d693
Leonardo Bishop <me@leonardobishop.com>
2023-09-20 14:00:10 +0100

    Update TokenAuthorizer to use Role enum

--- src/main/java/org/kainos/ea/auth/TokenAuthorizer.java
+++ src/main/java/org/kainos/ea/auth/TokenAuthorizer.java
@@ -1,21 +1,14 @@
 package org.kainos.ea.auth;
 
-import io.dropwizard.auth.AuthenticationException;
-import io.dropwizard.auth.Authenticator;
 import io.dropwizard.auth.Authorizer;
 import org.kainos.ea.cli.Role;
 import org.kainos.ea.cli.User;
-
-import javax.annotation.Nullable;
-import javax.ws.rs.container.ContainerRequestContext;
-import java.security.Principal;
-import java.util.Optional;
 
 public class TokenAuthorizer implements Authorizer<User> {
 
     @Override
     public boolean authorize(User user, String role) {
-        return "Admin".equals(user.getRole().toString()) || role.equals(user.getRole().toString());
+        return Role.ADMIN.toString().equals(user.getRole().toString()) || role.equals(user.getRole().toString());
     }
 
 }
