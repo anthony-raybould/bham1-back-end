c1582760c0cc199c47c639daa5f7c8a8b7f214c7
robbiecrabtree <robbie.crabtree@kainos.com>
2023-09-26 14:54:12 +0100

    fixed rebase

--- src/main/java/org/kainos/ea/DropwizardWebServiceApplication.java
+++ src/main/java/org/kainos/ea/DropwizardWebServiceApplication.java
@@ -1,24 +1,21 @@
 package org.kainos.ea;
 
 import io.dropwizard.Application;
+import io.dropwizard.auth.Auth;
 import io.dropwizard.auth.AuthDynamicFeature;
 import io.dropwizard.auth.AuthValueFactoryProvider;
 import io.dropwizard.setup.Bootstrap;
 import io.dropwizard.setup.Environment;
 import io.federecio.dropwizard.swagger.SwaggerBundle;
 import io.federecio.dropwizard.swagger.SwaggerBundleConfiguration;
-import org.kainos.ea.api.JobRoleService;
-import org.kainos.ea.db.BandDao;
-import org.kainos.ea.db.CapabilityDao;
-import org.kainos.ea.db.DatabaseConnector;
+import org.checkerframework.checker.units.qual.A;
+import org.kainos.ea.api.*;
+import org.kainos.ea.auth.*;
+import org.kainos.ea.db.*;
 import org.kainos.ea.resources.AuthController;
 import org.glassfish.jersey.server.filter.RolesAllowedDynamicFeature;
-import org.kainos.ea.auth.TokenAuthFilter;
-import org.kainos.ea.auth.TokenAuthenticator;
-import org.kainos.ea.auth.TokenAuthorizer;
 import org.kainos.ea.cli.User;
 import org.kainos.ea.api.JobRoleService;
-import org.kainos.ea.db.JobRoleDao;
 import org.kainos.ea.resources.BandController;
 import org.kainos.ea.resources.CapabilityController;
 import org.kainos.ea.resources.JobRoleController;
@@ -48,17 +45,34 @@ public class DropwizardWebServiceApplication extends Application<DropwizardWebSe
     @Override
     public void run(final DropwizardWebServiceConfiguration configuration,
                     final Environment environment) {
-        final DatabaseConnector databaseConnector = new DatabaseConnector();
 
+        final DatabaseConnector databaseConnector = new DatabaseConnector();
+        final UpdateJobRoleValidator jobRoleValidator = new UpdateJobRoleValidator();
         final JobRoleDao jobRoleDao = new JobRoleDao(databaseConnector);
-        final JobRoleService jobRoleService = new JobRoleService(jobRoleDao);
+        final JobRoleService jobRoleService = new JobRoleService(jobRoleDao, jobRoleValidator);
         final CapabilityDao capabilityDao = new CapabilityDao(databaseConnector);
         final BandDao bandDao = new BandDao(databaseConnector);
         final CapabilityService capabilityService = new CapabilityService(capabilityDao);
         final BandService bandService = new BandService(bandDao);
+        final AuthDao authDao = new AuthDao(databaseConnector);
+        final JWTService jwtService = new JWTService();
+        final TokenService tokenService = new TokenService(authDao, jwtService);
+        final AuthService authService = new AuthService(authDao, tokenService);
+        environment.jersey().register(new AuthController(authService));
         environment.jersey().register(new JobRoleController(jobRoleService));
         environment.jersey().register(new BandController(bandService));
         environment.jersey().register(new CapabilityController(capabilityService));
+
+        environment.jersey().register(new AuthDynamicFeature(
+                new TokenAuthFilter.Builder()
+                        .setAuthenticator(new TokenAuthenticator(tokenService))
+                        .setAuthorizer(new TokenAuthorizer())
+                        .setPrefix("Bearer")
+                        .buildAuthFilter()));
+        environment.jersey().register(RolesAllowedDynamicFeature.class);
+        environment.jersey().register(new AuthValueFactoryProvider.Binder<>(User.class));
+        environment.jersey().register(RolesAllowedDynamicFeature.class);
+        environment.jersey().register(new AuthValueFactoryProvider.Binder<>(User.class));
     }
 
 }
